ed129a6cb6209f942dc369868433a80c335c71fc
Leonardo Quintero <6802237+leonardoi7@users.noreply.github.com>
2024-01-15 21:59:14 -0800

    Initial Commit

---
+++ Bitso/BitsoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitsoApp.swift
+//  Bitso
+//
+//  Created by leonardo on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BitsoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
